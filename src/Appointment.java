b29328b87e2b2f94543dde7a9d38baf39cbe529d
camhoss <82007764+camhoss@users.noreply.github.com>
2021-04-18 18:03:17 -0400

    Update Appointment.java
    Set up the methods and variables. Need to Date first before I implement these methods

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,2 +1,32 @@
+/**
+  4/18/2021
+  This class models appointments
+  @author Cameron Hoss
+*/
 public class Appointment {
+  /*
+  Instance Variables
+  */
+  private Date date;
+  private String aliment;
+  private Date followUp;
+  
+  /*
+  Constructors
+  */
+  public Appointment()
+  {
+  }
+  
+  /*
+  Methods
+  */
+  public void Appointment(Date date, String str)
+  {
+    //???
+  }
+  public void setFollowUp(Date date)
+  {
+    //???
+  }
 }
