f1fdb6f60acfeb6c0906ee213af15af190fff143
camhoss <82007764+camhoss@users.noreply.github.com>
2021-04-18 18:49:15 -0400

    Done

--- src/Insurance.java
+++ src/Insurance.java
@@ -1,4 +1,39 @@
+/**
+4/18/2021
+This class will model our patient's insurance
+@author Cameron Hoss
+*/
 public class Insurance
 {
-  //insurance class
-}
+  /*
+  Instance Variables
+  */
+  private String cardHolder;
+  private String id;
+  private String groupNum;
+  
+  /*
+  Constructors
+  */
+  public Insurance(String cardHolder, String id, String groupNum)
+  {
+    this.cardHolder = cardHolder;
+    this.id = id;
+    this.groupNum = groupNum;
+  }
+  
+  /*
+  Methods
+  */
+  public String getCardHolder()
+  {
+    return cardHolder;
+  }
+  public String getID()
+  {
+    return id;
+  }
+  public String getGroupNum()
+  {
+    return groupNum;
+  }
