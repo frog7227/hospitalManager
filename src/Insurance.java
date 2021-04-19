c9e41bbb2e14f4bdd7561884c3c114da84eb9c7d
Andrew Ostrosky <frog7227@gmail.com>
2021-04-19 17:16:22 -0400

    Added end brace

--- src/Insurance.java
+++ src/Insurance.java
@@ -3,47 +3,45 @@
 This class will model our patient's insurance
 @author Cameron Hoss
 */
-public class Insurance
-{
+public class Insurance {
   /*
   Instance Variables
   */
   private String cardHolder;
   private String id;
   private String groupNum;
-  
+
   /*
   Constructors
   */
-  public Insurance(String cardHolder, String id, String groupNum)
-  {
+  public Insurance(String cardHolder, String id, String groupNum) {
     this.cardHolder = cardHolder;
     this.id = id;
     this.groupNum = groupNum;
   }
-  
+
   /*
   Methods
   */
-  public String getCardHolder()
-  {
+  public String getCardHolder() {
     return cardHolder;
   }
-  public String getID()
-  {
+
+  public String getID() {
     return id;
   }
-  public String getGroupNum()
-  {
+
+  public String getGroupNum() {
     return groupNum;
   }
+
   /**
-  newInsurance
-  This method takes in parameters to overwrite the current instance variables
-  */
-  public void newInsurnace(String cardHolder, String id, String groupNum)
-  {
+   * newInsurance
+   * This method takes in parameters to overwrite the current instance variables
+   */
+  public void newInsurnace(String cardHolder, String id, String groupNum) {
     this.cardHolder = cardHolder;
     this.id = id;
     this.groupNum = groupNum;
   }
+}
