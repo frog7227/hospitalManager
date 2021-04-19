25728be1a3028d84d5fab5af7bdc271b28cb379c
Cameron Hoss <cameronmhoss@gmail.com>
2021-04-19 17:24:14 -0400

    Made Medication look not terrible

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,9 +1,8 @@
 /**
- * 4/18/2021
- * This class models appointments
- *
- * @author Cameron Hoss
- */
+  4/18/2021
+  This class models appointments
+  @author Cameron Hoss
+*/
 public class Appointment {
     /*
     Instance Variables
@@ -11,19 +10,21 @@ public class Appointment {
     private Date date;
     private String aliment;
     private Date followUp;
-
+  
     /*
     Constructors
     */
-    public Appointment(Date date, String aliment) {
-        this.date = date;
-        this.aliment = aliment;
+    public Appointment(Date date, String aliment)
+    {
+      this.date = date;
+      this.aliment = aliment;
     }
-
+  
     /*
     Methods
     */
-    public void setFollowUp(Date followUp) {
-        this.followUp = followUp;
+    public void setFollowUp(Date followUp)
+    {
+      this.followUp = followUp;
     }
 }
--- src/Medication.java
+++ src/Medication.java
@@ -1,13 +1,10 @@
 /**
- * 4/18/2021
- * This class will model perscribed medications
- *
- * @author Cameron Hoss
- */
+4/18/2021
+This class will model perscribed medications
+@author Cameron Hoss
+*/
 public class Medication {
-    //Saves you from calling getDose and getDoseUnit back to back
-
-    /*
+    /**
     Instance Variables
     */
     private String name;
@@ -15,44 +12,49 @@ public class Medication {
     private String doseUnit;
     private Date startDate;
     private Date endDate;
-
-
-    /*
+  
+    /**
     Constructors
     */
-    public Medication(String name, double dose, String doseUnit, Date startDate, Date endDate) {
-        this.name = name;
-        this.dose = dose;
-        this.doseUnit = doseUnit;
-        this.startDate = startDate;
-        this.endDate = endDate;
+    public Medication(String name, double dose, String doseUnit, Date startDate, Date endDate)
+    {
+      this.name = name;
+      this.dose = dose;
+      this.doseUnit = doseUnit;
+      this.startDate = startDate;
+      this.endDate = endDate;
     }
-
-    /*
+  
+    /**
     Methods
     */
-    public String getName() {
-        return name;
+    public String getName()
+    {
+      return name;
+    }
+    public double getDose()
+    {
+      return dose;
+    }
+    public String getDoseUnit()
+    {
+      return doseUnit;
+    }
+    public Date getStartDate()
+    {
+      return startDate;
+    }
+    public Date getEndDate()
+    {
+      return endDate;
     }
 
-    public double getDose() {
-        return dose;
-    }
-
-    public String getDoseUnit() {
-        return doseUnit;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
+  /**
+   * doseOutput
+   * Saves you from calling getDose and getDoseUnit back to back
+   */
     public String doseOutput()
     {
-        return dose + " " + doseUnit;
+      return dose + " " + doseUnit;
     }
 }
