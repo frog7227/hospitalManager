aac662d26e575bef341da9fe13bcf85347c079a5
Andrew Ostrosky <frog7227@gmail.com>
2021-04-19 17:29:50 -0400

    removed extra @author tag

--- src/PatientNotFoundException.java
+++ src/PatientNotFoundException.java
@@ -2,9 +2,6 @@
  * @author Andrew Ostrosky
  */
 public class PatientNotFoundException extends Exception {
-    /**
-     * @author Andrew Ostrosky
-     */
     public PatientNotFoundException() {
 
         super("The patient could not be found in the database");
