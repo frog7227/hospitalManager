beb73180b4ac5ddcce7371a334a5f6046dc8097c
Andrew Ostrosky <frog7227@gmail.com>
2021-05-03 20:02:57 -0400

    Fixed formatting in Exception
    Signed-off-by: Andrew Ostrosky <frog7227@gmail.com>

--- src/models/exceptions/PatientUnableToBeSavedException.java
+++ src/models/exceptions/PatientUnableToBeSavedException.java
@@ -5,6 +5,7 @@ package models.exceptions;
  */
 public class PatientUnableToBeSavedException extends Exception {
     public PatientUnableToBeSavedException() {
+
         super("The Patient couldn't be saved to the Database.");
     }
 }
